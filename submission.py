df3f697c86e41fb075790abae280ccf9779e926d
Chris Rellama <142788291+chrisrellama@users.noreply.github.com>
2024-05-01 10:29:36 +0800

    Update submission.py

--- submission.py
+++ submission.py
@@ -41,15 +41,15 @@ def clean_df(df, background_df=None):
     # df["age"] = 2024 - df["birthyear_bg"]
     df = df.copy()
 
-    file_path = "low_importance_features.txt"
+    # file_path = "low_importance_features.txt"
 
     # # Initialize an empty list to store the feature names
-    loaded_feature_names = []
+    # loaded_feature_names = []
 
     # Open the file in read mode
-    with open(file_path, "r") as file:
-        for line in file:
-            loaded_feature_names.append(line.strip())
+    # with open(file_path, "r") as file:
+        # for line in file:
+            # loaded_feature_names.append(line.strip())
 
     # df.drop('nomem_encr', axis=1, inplace=True)
 
@@ -104,8 +104,10 @@ def clean_df(df, background_df=None):
 
     # df.drop('nomem_encr', axis=1, inplace=True)
 
-    columns_to_keep = [col for col in df.columns if col not in loaded_feature_names]
-    df = df[columns_to_keep]
+    # columns_to_keep = [col for col in df.columns if col not in loaded_feature_names]
+    # df = df[columns_to_keep]
+
+    df = df[cols_to_keep]
 
     return df
 
