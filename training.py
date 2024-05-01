6b5b5188fc4c278fdafe07dd4dc9a1b65cd36b5a
Chris Rellama <142788291+chrisrellama@users.noreply.github.com>
2024-05-01 19:55:26 +0800

    Update training.py

--- training.py
+++ training.py
@@ -8,6 +8,7 @@ It is important to document your training steps here, including seed,
 number of folds, model, et cetera
 """
 from xgboost import XGBClassifier
+from sklearn.ensemble import RandomForestClassifier
 
 def train_save_model(cleaned_df, outcome_df):
     """
@@ -30,18 +31,20 @@ def train_save_model(cleaned_df, outcome_df):
     # Logistic regression model
     # model = LogisticRegression()
 
-    from xgboost import XGBClassifier
+    # from xgboost import XGBClassifier
 
     # Best parameters obtained from grid search
-    best_params = {
-        'learning_rate': 0.3,
-        'max_depth': 7,
-        'min_child_weight': 5,
-        'subsample': 0.9,
-        'n_estimators': 100
-    }
+    # best_params = {
+    #     'learning_rate': 0.3,
+    #     'max_depth': 7,
+    #     'min_child_weight': 5,
+    #     'subsample': 0.9,
+    #     'n_estimators': 100
+    # }
 
-    model = XGBClassifier(**best_params, random_state=1)
+    # model = XGBClassifier(**best_params, random_state=1)
+
+    model = RandomForestClassifier(random_state=1)
 
     # Fit the model
     # model.fit(model_df[['age']], model_df['new_child'])
