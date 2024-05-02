6c93d569839c3b048e95d2f1fb0d07ddaa34b096
Chris Rellama <142788291+chrisrellama@users.noreply.github.com>
2024-05-02 18:51:38 +0800

    Update training.py

--- training.py
+++ training.py
@@ -8,7 +8,7 @@ It is important to document your training steps here, including seed,
 number of folds, model, et cetera
 """
 from xgboost import XGBClassifier
-from sklearn.ensemble import RandomForestClassifier
+# from sklearn.ensemble import RandomForestClassifier
 
 def train_save_model(cleaned_df, outcome_df):
     """
@@ -31,30 +31,30 @@ def train_save_model(cleaned_df, outcome_df):
     # Logistic regression model
     # model = LogisticRegression()
 
-    # from xgboost import XGBClassifier
+    from xgboost import XGBClassifier
 
-    # Best parameters obtained from grid search
-    # best_params = {
-    #     'learning_rate': 0.3,
-    #     'max_depth': 7,
-    #     'min_child_weight': 5,
-    #     'subsample': 0.9,
-    #     'n_estimators': 100
-    # }
+    Best parameters obtained from grid search
+    best_params = {
+        'learning_rate': 0.3,
+        'max_depth': 7,
+        'min_child_weight': 5,
+        'subsample': 0.9,
+        'n_estimators': 100
+    }
 
-    # model = XGBClassifier(**best_params, random_state=1)
+    model = XGBClassifier(**best_params, random_state=1)
 
-    model = RandomForestClassifier(random_state=1)
+    # model = RandomForestClassifier(random_state=1)
 
     # Fit the model
     # model.fit(model_df[['age']], model_df['new_child'])
 
     X = model_df.drop(columns=['new_child'], axis=1)
 
-    # y = model_df['new_child']
+    y = model_df['new_child']
 
     # Train the model with the best parameters
-    model.fit(X, model_df['new_child'])
+    model.fit(X, y)
 
     # Save the model
     joblib.dump(model, "model.joblib")
